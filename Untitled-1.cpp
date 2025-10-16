99af215c9c55bb1d967ba5e059e5f127e08531e0
limpicolezero-droid <limpicolezero@gmail.com>
2025-10-16 08:32:51 -0300

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double n1, n2, notaFinal;
+    cout << "Digite as duas notas: ";
+    cin >> n1 >> n2;
+
+    notaFinal = n1 + n2;
+    cout << fixed << setprecision(1);
+    cout << "NOTA FINAL = " << notaFinal << endl;
+
+    if (notaFinal < 60.0)
+        cout << "REPROVADO" << endl;
+
+    return 0;
+}
